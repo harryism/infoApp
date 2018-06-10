aac4545f927c64b2977a49c8ba67afdec76c3d49
harryism <hisma038@uottawa.ca>
2018-06-10 15:56:19 -0400

    added validations to name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  
+  validates :name, presence: true, 
+            length: {minimum: 2, maximum:25}
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+            length: {maximum:100}, format: { with: VALID_EMAIL_REGEX}
 end
